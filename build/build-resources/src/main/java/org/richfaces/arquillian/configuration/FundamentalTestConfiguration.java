7986aa1231ba6793463f62b3dd1928cfe06ff064
Alberto Fernandez <infjaf@gmail.com>
2017-06-05 21:56:05 +0200

    Next release cycle

--- build/build-resources/src/main/java/org/richfaces/arquillian/configuration/FundamentalTestConfiguration.java
+++ build/build-resources/src/main/java/org/richfaces/arquillian/configuration/FundamentalTestConfiguration.java
@@ -10,7 +10,7 @@ public class FundamentalTestConfiguration implements DroneConfiguration<Fundamen
 
     private String richfacesVersion;
     private Boolean servletContainerSetup;
-    private String currentBuildRichfacesVersion = "4.5.19.ayg";
+    private String currentBuildRichfacesVersion = "4.5.20-SNAPSHOT";
     private String jsfProvider;
     private String jsfImplementation;
     private String container;
--- components/rich/src/test/integration/org/richfaces/integration/partialViewContext/ITStatelessViews.java
+++ components/rich/src/test/integration/org/richfaces/integration/partialViewContext/ITStatelessViews.java
@@ -47,7 +47,7 @@ public class ITStatelessViews {
     public static WebArchive createDeployment() {
         RichDeployment deployment = new RichDeployment(ITStatelessViews.class);
 
-        deployment.addMavenDependency("org.richfaces:richfaces-a4j:4.5.19.ayg");
+        deployment.addMavenDependency("org.richfaces:richfaces-a4j:4.5.20-SNAPSHOT");
 
         addIndexPage(deployment);
 
